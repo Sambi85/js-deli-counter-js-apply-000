9f5d7901ce51009e07b41b828ccc709a8e4f4c0d
Samuel Lesser <ineedmoreplates@gmail.com>
2020-01-15 21:39:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
-const person = [];
-var i = [];
-
+var number = 1;
 
 function takeANumber(katzDeliLine,person) {
-    katzDeliLine.push(person);
-    if (i >= 0) { [i]+1;
-    for (i = 0; i < katzDeliLine.length; i++) {
-    return String("Welcome, "+person+". You are number "+[katzDeliLine.length]+" in line.");}
-  }
+  katzDeliLine.push(person);
+    return `Welcome, ${person}. You are number ${katzDeliLine.length} in line.`;
 }
+ 
+function takeANumber(katzDeliLine) { 
+  katzDeliLine.push(number);
+  number++;
+  return `Welcome you are ${number} in line.`; }
 
-
+  
 function nowServing(katzDeliLine) {
-        
-        if ([i] < katzDeliLine.length) {
-        for (i= 0; i < katzDeliLine.length; i++) { 
-        var ticket = 'Currently serving '+katzDeliLine[i]+'.';
-        katzDeliLine.shift(0);
-        return ticket;}
+   const firstPersonInLine = katzDeliLine.shift();
+  if (katzDeliLine.length) {
+    return `Currently serving ${firstPersonInLine}.`;}
+      else { return "There is nobody waiting to be served!" } 
+}
+   
+function currentLine(katzDeliLine) { 
+  const line = [];  
+     if (katzDeliLine.length) {
+      for (let i= 0; i < katzDeliLine.length; i++) 
+        { line.push(` ${[i+1]}. ${katzDeliLine[i]}`); }
+            return `The line is currently:${line}`;}
           
-                }   else if ([i] < 0); { return "There is nobody waiting to be served!" }
-          }
-          
-          
-function currentLine(line) {
-     if ([i] < line.length) {
-      var list = [" "+[i+1]+'. '+line[i]," "+[i+2]+'. '+line[i+1]," "+[i+3]+'. '+line[i+2]];
-       for (i=0; i < list.length; i++)
-          return String('The line is currently:'+list);
-
-        } else if ([i] < 0); { 
-
-        return "The line is currently empty.";}
-        }
+       else {
+         return "The line is currently empty.";}
+} 
